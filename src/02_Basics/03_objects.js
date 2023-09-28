6e463cda55b835c2fef8529ef2272e0ab38110f9
Sarfaraz Hussain Chaudhary <message.sarfarazhussain@yahoo.com>
2023-09-28 20:33:35 +0530

    Initial commit

---
+++ src/02_Basics/03_objects.js
@@ -0,0 +1,11 @@
+// Object literals
+const JsUser = {
+  name: "Hitesh",
+  age: 18,
+  location: "Jaipur",
+  email: "hitesh@google.com",
+  isLoggedIn: false,
+  lastLoginDay: ["Monday", "Saturday"]
+};
+
+console.log(JsUser["name"]);
